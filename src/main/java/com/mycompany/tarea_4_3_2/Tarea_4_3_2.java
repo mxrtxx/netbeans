8988a2c159b7affb24c42cf53f650096ece061a6
AlumnoMañana <AlumnoMaana@DESKTOP-KK9UC82>
2024-02-07 09:56:31 +0100

    Este código imprime Hello World!

---
+++ src/main/java/com/mycompany/tarea_4_3_2/Tarea_4_3_2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tarea_4_3_2;
+
+/**
+ *
+ * @author AlumnoMañana
+ */
+public class Tarea_4_3_2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
